10e0d44a8efc11cf1980c24faecaabd45b900d83
luis <luisxhl7@gmail.com>
2022-08-23 10:34:06 -0500

    se incluye a la interfaz que sea response

--- src/login.js
+++ src/login.js
@@ -7,14 +7,14 @@ iconOjo.addEventListener("click",function(){
   =====================================================*/ 
   const icon = this.querySelector("i");
 
-    if(this.nextElementSibling.type === "password"){
-      this.nextElementSibling.type = "text"; //campo de texto 
-      icon.classList.remove("fa-eye-slash"); //ojo bloqueado
-      icon.classList.add("fa-eye");          //ojo abierto
-    }
-    else if(this.nextElementSibling.type === "text"){
-      this.nextElementSibling.type = "password"; //campo de password
-      icon.classList.remove("fa-eye");       //ojo abierto
-      icon.classList.add("fa-eye-slash");    //ojo bloqueado
-    }
+  if(this.nextElementSibling.type === "password"){
+    this.nextElementSibling.type = "text"; //campo de texto 
+    icon.classList.remove("fa-eye-slash"); //ojo bloqueado
+    icon.classList.add("fa-eye");          //ojo abierto
+  }
+  else if(this.nextElementSibling.type === "text"){
+    this.nextElementSibling.type = "password"; //campo de password
+    icon.classList.remove("fa-eye");       //ojo abierto
+    icon.classList.add("fa-eye-slash");    //ojo bloqueado
+  }
 })
